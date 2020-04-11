e97d733eb20ed437500d067b8129b256571a21aa
TL <TL@gmail.com>
2020-04-11 19:00:47 +0530

    after stash finally work done

--- DemoProj1/src/com/nit/beans/Utility.java
+++ DemoProj1/src/com/nit/beans/Utility.java
@@ -16,8 +16,14 @@ public class Utility {
 		System.out.println("Utility.m2()");
 	}
 	
+
 	public void m4() {
 		System.out.println("Utility.m4()...after stashing TL give to do this..");
 		System.out.println("Utility.m4()...after change done by dev1...");
+	}
+
+	public void m3() {
+		System.out.println("Utility.m3()...stash...after done m3 work you send it to TL");
+
 	}
 }
