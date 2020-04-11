e62197c79246277b7003956a765bc28b2e8462a5
Dev1 <Dev1@gmail.com>
2020-04-11 09:25:47 +0530

    dev1 modified test.java

--- DemoProj1/src/com/nit/test/Test.java
+++ DemoProj1/src/com/nit/test/Test.java
@@ -1,5 +1,11 @@
 package com.nit.test;
 
+import com.nit.beans.Utility;
+
 public class Test {
+	public static void main(String[] args) {
+		Utility u =new Utility();
+		System.out.println(u.getSysDate());
+	}
 
 }
